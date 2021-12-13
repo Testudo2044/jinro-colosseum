93c8900d0c7fc3edfc8cad6fd6f9bcb22a888343
Testudo2044 <68946985+Testudo2044@users.noreply.github.com>
2021-12-13 09:01:26 +0900

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,71 @@
+
+import Link from 'next/link'
+import React from 'react';
+
+
+
+
+const HeaderFunction: React.FC = () =>
+  <div className="">
+    <div className="smartStyle">
+
+    </div>
+
+    <div className="hidden lg:block PCStyle">
+      <div className="bg-black ">
+        <ul className="flex text-2xl text-white">
+          <li className="lg:hover:text-3xl">
+            <Link href="/">
+              <a><ruby>ABOUT<rt>UN人狼とは</rt></ruby></a>
+            </Link>
+          </li>
+          <li className="lg:hover:text-3xl">
+            <Link href="/">
+            <a><ruby>RULE<rt>ゲームルール</rt></ruby></a>
+          </Link>
+          </li>
+          <li className="lg:hover:text-3xl">
+            <Link href="/">
+              <a><ruby>SET<rt>販売</rt></ruby></a>
+            </Link>
+          </li>
+          <li className="lg:hover:text-3xl">
+            <Link href="/">
+              <a><ruby>CHARACTER<rt>役職紹介</rt></ruby></a>
+            </Link>
+          </li>
+          <li className="lg:hover:text-3xl">
+            <Link href="/">
+              <a><ruby>MEMBER<rt>メンバー</rt></ruby></a>
+            </Link>
+          </li>
+          <li className="lg:hover:text-3xl">
+            <Link href="/">
+              <a><ruby>COMTACT<rt>お問い合わせ</rt></ruby></a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>;
+
+const MainFunction: React.FC = () =>
+  <div>
+
+  </div>;
+
+const FooterFunction: React.FC = () =>
+  <div>
+    <h1>hogehoge</h1>
+  </div>;
+
+
+export default function Index() {
+  return (
+    <div>
+      <HeaderFunction />
+      <MainFunction />
+      <FooterFunction />
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+   // 追記
+  mode: 'jit',
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
